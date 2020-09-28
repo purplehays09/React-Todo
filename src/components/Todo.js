65117146b681123410f70ca184a6a5d0e33bcafa
david hays <purplehays527@gmail.com>
2020-09-28 14:20:46 -0600

    added the other components

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,2 +1,14 @@
 import React from 'react'
 
+const Item = (props) => {
+  return (
+    <div
+      onClick={() => props.toggleCompleted(props.item.id)}
+      className={`item${props.item.completed ? " completed" : ""}`}
+    >
+      <p>{props.item.name}</p>
+    </div>
+  );
+};
+
+export default Item;
