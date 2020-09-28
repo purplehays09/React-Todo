0a60ad12fbd1d0f5ea1e05608e1cecb3bd851d06
david hays <purplehays527@gmail.com>
2020-09-28 12:39:59 -0600

    added the constructor and the three actions, toggleCHecked, addItem, and clearCompleted

--- src/App.js
+++ src/App.js
@@ -4,6 +4,53 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(){
+    super();
+    this.state = {
+      items:[]
+    }
+  }
+
+  toggleChecked = (clickedId) => {
+    this.setState({
+
+      items: this.state.items.map(item => {
+        if(item.id === clickedId){
+          return{
+            ...item,
+            completed: !item.completed
+          }
+        }else{
+          return item
+        }
+      })
+    }
+    )
+  }
+
+  addItem = itemName => {
+    const newItem = {
+      name:itemName,
+      id: new Date(),
+      completed:false
+    }
+
+    this.setState({
+      items:[...this.state.items,newItem]
+    })
+  }
+
+  clearCompleted = (event) => {
+    event.preventDefault()
+    this.setState({
+      items:this.state.items.filter(item => {
+        if(item.completed === false){
+          return item
+        }
+      })
+    })
+  }
   render() {
     return (
       <div>
